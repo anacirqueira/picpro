c87c69cb0c36ea11d8b4bf055caed5dad7859016
anacirqueira <cirqueiraana6@gmail.com>
2022-02-27 01:20:56 -0300

    aula javascript

---
+++ js/aula_javaScript.js
@@ -0,0 +1,147 @@
+// VARIAVEIS
+
+/* let nome = "Ana Cirqueira"
+let pessoa1 = "Mayara"
+
+const idade = 10 */
+
+// NUMEROS
+
+/* let numero1 = 400
+let numero2 = 10 */
+//let resultado = numero1+numero2
+
+
+// TIPOS DE DADOS
+
+//STRINGS
+
+/* let primeiroNome = "Ana"
+let segundoNome = "Paula" */
+
+//INTEIROS
+
+/* let primeiroNumero = 10
+let segundoNumero = 30 */
+
+// Tipos de dados => Objetos:
+
+//ARRAY
+
+/* let listaNomes = []
+listaNomes.push("Ana")
+
+let listaNumeros = [1,2,3,4,5] */
+
+//OBJETO
+
+/* const amanda = {
+  nome: "Amanda",
+  telefone: "999999",
+  cpf: "44468765468",
+  cidade: "Curitiba",
+}
+
+const Ana = {
+  nome: "Ana",
+  telefone: "11940847866",
+  cpf: "123456789",
+  cidade: "São Paulo",
+} */
+
+
+//Operadores básicos
+//SOMA            => +
+//SUBTRAÇÃO       => -
+//DIVISÃO         => /
+//MULTIPLICAÇÃO   => *
+//MAIS IGUAL      => +=
+//MENOS IGUAL     => -=
+
+
+//Booleanos e comparações
+//MAIOR 10 > 10       => FALSE
+//MENOR 1 < 10        => TRUE
+//IGUALDADE 10 == 10  => TRUE
+//DIFERENTE 10 !== 12 => TRUE
+//VERIFICA IGUALDADE E TBM O TIPO DE DADO (===) "10" => FALSE
+
+//Condicionais (if/else)
+
+/* const ana2 = {
+  nome: "Ana",
+  idade: 26,
+  cidade: "São Paulo"
+} */
+
+// && (E) AS DUAS VERIFICAÇÕES PRECISAM SER VERDADEIRAS
+// || (OU) QUALQUER UMA PRECISA SER VERDADEIRA
+
+// if(ana2.idade >= 18){
+//    console.log("Essa pessoa atingiu a maioridade")
+//
+//    if(ana2.cidade == "Carapicuiba"){
+//      console.log("Cidade correta")
+//   }else{
+//      console.log("Cidade não correspondente")
+//    }
+// }else{
+//  console.log("Essa pessoa não atingiu a maioridade")
+// }
+
+
+// FUNÇÕES NO JAVASCRIPT
+
+//DECLARANDO
+
+/* function soma(numero1,numero2){
+
+  console.log(numero1 + numero2)
+}
+//CHAMANDO FUNÇÃO
+soma(24,46) */
+
+
+//return
+
+/* function divisao(num1, num2){
+
+  return num1/num2
+
+}
+let resutado = divisao(20,4)
+ */
+//Estrutura de repetição
+
+/* let filaAtendimento = ["Ana", "Mayara", "Greg", "Jean", "Gabriel"]
+
+//PARA
+for(let i = 0; i < filaAtendimento; i++){
+
+  console.log(filaAtendimento[i])
+} */
+
+
+//*******************************************/
+//       DOM = MODELO DE OBJETO DO DOCUMENTO
+//******************************************/
+
+//documento => DOCUMENTO HTML
+//querySelector => SELETOR DE ELEMENTO
+
+//let divPessoa = document.querySelector(".pessoa")
+
+//let filaAtendimento = ["Hudson", "Vitor", "Guilherme", "Amanda", "Maria", "Daniel","Gabriel"]
+// for(let i = 0; i < filaAtendimento.; i = i + 1){
+    
+    
+//     let div = document.createElement("div")
+//     div.innerText = filaAtendimento[i]
+
+//     //divPessoa.appendChild(div)
+  
+// }
+
+
+//AULA DE DOM 
+//SELETORES
